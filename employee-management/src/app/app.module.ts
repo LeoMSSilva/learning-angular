91459d472f1f1243eadb88e4c27ee4650abc7270
LeoMSSilva <l.eom1994@hotmail.com>
2022-06-18 02:49:16 -0300

    Feat: created modules (shared and features) and components (employees, header and footer)

--- employee-management/src/app/app.module.ts
+++ employee-management/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FeaturesModule } from './features/features.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FeaturesModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
