f31f14f0fae4d415c2e197d68220b77394ef04bd
LeoMSSilva <l.eom1994@hotmail.com>
2022-06-18 03:14:02 -0300

    Feat: filled information and styled components (employees, header and footer)

--- employee-management/src/app/features/employees/employees.component.ts
+++ employee-management/src/app/features/employees/employees.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-employees',
   templateUrl: './employees.component.html',
-  styleUrls: ['./employees.component.scss']
+  styleUrls: ['./employees.component.scss'],
 })
 export class EmployeesComponent implements OnInit {
+  title: string = 'Gerenciamento de funcionários';
+  employees = {
+    id: 1,
+    name: 'Leonardo',
+    wage: 10000,
+    office: 'Developer',
+  };
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
