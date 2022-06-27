dbc6c5438e054b94f0e11c86e85be4a7337ee4ad
LeoMSSilva <l.eom1994@hotmail.com>
2022-06-27 20:55:58 -0300

    Feat: implemented ngif, ngfor and ngclass directives in employees project and changed table style

--- employee-management/src/app/features/components/employees/employees.component.ts
+++ employee-management/src/app/features/components/employees/employees.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+interface IEmployee {
+  id: number;
+  name: string;
+  wage: number;
+  office: string;
+  photo: string;
+}
+
 @Component({
   selector: 'app-employees',
   templateUrl: './employees.component.html',
@@ -7,13 +15,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeesComponent implements OnInit {
   title: string = 'Gerenciamento de funcionários';
-  employees = {
-    id: 1,
-    name: 'Leonardo',
-    wage: 10000,
-    office: 'Developer',
-  };
+
+  employees: IEmployee[] = [
+    {
+      id: 1,
+      name: 'Leonardo',
+      wage: 10000,
+      office: 'Developer',
+      photo: 'https://www.github.com/LeoMSSilva.png',
+    },
+    {
+      id: 2,
+      name: 'Nathan',
+      wage: 10000,
+      office: 'Developer',
+      photo: 'https://www.github.com/NathanCarlos.png',
+    },
+    {
+      id: 3,
+      name: 'Alan',
+      wage: 10000,
+      office: 'Developer',
+      photo: 'https://www.github.com/alanjhonnes.png',
+    },
+  ];
+
+  employee: number = 0;
+
+  visible: boolean = false;
+
   constructor() {}
+
+  employeeSelect(id: number): void {
+    this.visible = !this.visible;
+    this.employee = id;
+  }
 
   ngOnInit(): void {}
 }
