36557ba7c3f9912eaccecbeb8e12e9f1328af953
LeoMSSilva <l.eom1994@hotmail.com>
2022-07-24 21:40:35 -0300

    Feat: implemented routing in header

--- academyStudentManagement/src/app/app-routing.module.ts
+++ academyStudentManagement/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './features/login/pages/login/login.component';
+import { RegisterComponent } from './features/register/pages/register/register.component';
+import { HomeComponent } from './features/home/pages/home/home.component';
+import { TasksComponent } from './features/tasks/pages/tasks/tasks.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'tasks', component: TasksComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
